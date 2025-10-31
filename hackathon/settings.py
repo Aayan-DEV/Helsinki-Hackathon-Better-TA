f15e4a62d9412ba479f6e00ae6f89c04fa13920c
Aayan-DEV <aayanasif1234@gmail.com>
2025-11-01 01:47:19 +0200

    Fixed Env issues

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import environ
 
+env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
